08db124c912410aaf38a51853ecdb1608e6d71c9
Beryl <beryl.schragger@gmail.com>
2013-06-18 12:19:45 -0400

    Add MadLibs file.

---
+++ madlibs.rb
@@ -0,0 +1,41 @@
+puts "Give me a name."
+name = gets.chomp
+puts "Give me another name."
+name2 = gets.chomp
+puts "Give me a past tense verb."
+past = gets.chomp
+puts "Give me another past tense verb."
+past2 = gets.chomp
+puts "Past tense verb again."
+past3 = gets.chomp
+puts "One more past tense verb."
+past4 = gets.chomp
+puts "Now for a present tense verb."
+verb = gets.chomp
+puts "And another present tense verb."
+verb2 = gets.chomp
+puts "Now for a noun."
+noun = gets.chomp
+puts "Another noun."
+noun2 = gets.chomp
+puts "And another noun."
+noun3 = gets.chomp
+puts "Yet another noun."
+noun4 = gets.chomp
+puts "Almost there with the nouns."
+noun5 = gets.chomp
+puts "Last noun!"
+noun6 = gets.chomp
+puts "I lied.  I want just one plural noun."
+plural = gets.chomp
+puts "How about an adverb?"
+adv = gets.chomp
+puts "And an adjective."
+adj = gets.chomp
+puts "Last but not least, give me a number."
+number = gets.chomp
+
+puts "At once #{name} knew that the #{noun} would never #{verb} #{adv}.
+Only the #{noun2} #{past} now.  #{name} #{past2} the #{adj} #{noun3} on the #{noun4}, and prepared to #{verb2}.
+All of a sudden, #{name2} #{past3} from behind the #{noun5}, and #{past4}, 'But wait!
+There are #{number} #{plural} in my #{noun6}!'"
